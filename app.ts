1e22c770c083cd7e2ff355b74e0b787b367b868b
Transcendence <baydindenys@ukr.net>
2023-06-15 19:43:25 +0300

    statuses and types in ts
    statuses and types in ts

--- app.ts
+++ app.ts
@@ -1,11 +1,84 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+let age = 50;
+let name = 'Max';
+let toggle = true;
+let empty = null;
+let notInitialize = undefined;
 
-function add(num1, num2) {
-    return num1 + num2;
+
+let anything:any;
+anything = -20;
+anything = 'Text';
+anything = {};
+
+
+let some:unknown;
+some = 'Text';
+let str: string;
+// Type guard
+if (typeof some === 'string') {
+    str = some;
 }
 
-button.addEventListener("click", function() {
-    console.log(add(+input1.value, +input2.value));
-});
+
+let person: [string, number];
+person = ['Max', 21]
+
+
+enum Toggle {ENABLE, DISABLE};
+const service = {
+    status: Toggle.ENABLE,
+};
+
+if (service.status === Toggle.ENABLE) {
+    console.log('It is active')
+}
+if (service.status === Toggle.DISABLE) {
+    console.log('It is disabled')
+}
+
+
+function showMessage(message: string):void {
+    console.log(message);
+  }
+  
+  
+  function calc(num1: number, num2: number):number {
+    return num1 + num2;
+  }
+  
+  function customError():never {
+    throw new Error('Error');
+  }
+
+type page1 = {
+title: string;
+  likes: number;
+  accounts: string[];
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  }
+}
+  const page1 = {
+    title: 'The awesome page',
+    likes: 100,
+    accounts: ['Max', 'Anton', 'Nikita'],
+    status: 'open',
+    details: {
+      createAt: new Date('2021-01-01'),
+      updateAt: new Date('2021-05-01'),
+    }
+  }
+  
+  type page2 = {
+    title: string;
+    likes: number;
+  accounts: string[];
+  status: string;
+}
+  const page2 = {
+    title: 'Python or Js',
+    likes: 5,
+    accounts: ['Alex'],
+    status: 'close',
+  }
